16a71bab26287224a817468256392e30c83012cb
RakeshKhari <rkhari8368@gmail.com>
2026-06-06 13:33:16 +0530

    Updated Driver for CI with latest changes

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -12,7 +13,22 @@ public class DriverFactory {
 
         WebDriverManager.chromedriver().setup();
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        // CI mode (GitHub Actions / Linux)
+        if (System.getenv("CI") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--remote-allow-origins=*");
+        }
+
+        // Optional stability flags (safe everywhere)
+        options.addArguments("--disable-extensions");
+
+        driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
     }
